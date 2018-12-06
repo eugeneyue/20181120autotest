4e50bdba35710eda2809b8db360cd2a199a6e400
Yujiang Yue <eugene.yue@gllue.com>
2018-12-06 12:14:44 +0800

    temporarily combine send email with email content into one function

---
+++ Shutdownchrome.py
@@ -0,0 +1,5 @@
+# import time
+#
+# def shutchromedown(interval=3):
+#     time.sleep(interval)                                                                    # 等待3秒
+#     qd.quit()                                                                               # 关闭浏览器
